a6691ab4d921ff965e62d8d00d9fe1080d979e1c
slawekggg <slawekggg@users.noreply.github.com>
2018-05-06 10:15:05 +0200

    6.05

---
+++ skrypcior.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import requests
+import jmespath
+
+
+def main():
+    """
+    """
+    try:
+        r = requests.get("http://127.0.0.1:5000?output=json")
+    except requests.exceptions.RequestException as e:
+        print(e)
+        exit(1)
+    if r.status_code != 200:
+        print("FAILED: wywolanie nie powiodlo sie: " + r.text)
+        exit(2)
+    print(r.text)
+    print(r.json())
+    checkOutput(r.json())
+
+
+def checkOutput(rj):
+    actual = jmespath.search('imie', rj)
+    expected = "Slawomir"
+    if actual != expected:
+        print("FAILED: We expected: " + expected + " but was " + actual)
+        exit(1)
+
+
+if __name__ == "__main__":
+    """
+    """
+    main()
