215badf0f8f99e8faff2d0670dfd2b254f7a8fad
slawekggg <slawekggg@users.noreply.github.com>
2018-05-06 14:18:03 +0200

    sdsd

--- test_ui/test_ui.py
+++ test_ui/test_ui.py
@@ -9,7 +9,7 @@ class TestFormater(unittest.TestCase):
     def test_plain_lowercase(self):
         driver = webdriver.Chrome()
         driver.get("http://127.0.0.1:5000/ui")
-        time.sleep(10)
+
         tekst = driver.find_element_by_xpath('/html/body/h1')
         self.assertEqual(tekst.text,"Witaj swiecie!!!!!!")
         driver.quit()
